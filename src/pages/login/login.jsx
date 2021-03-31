f07b2059975cb48e9464e0bd81e02874efbe950e
wzc <wzc>
2021-03-31 22:03:13 +0800

    Initial commit

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './login.less';
+
+class Login extends Component {
+  render() {
+    return (
+        <div>
+
+        </div>
+    );
+  }
+}
+
+export default Login;
